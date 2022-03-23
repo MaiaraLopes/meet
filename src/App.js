6d477b95d6e98dedd895359448338891dc9709ce
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-03-23 18:33:27 +0000

    Implementing integration testing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import Event from "./Event";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents } from "./api";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+  };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter(
+        (event) => event.location === location
+      );
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
         <Event />
         <NumberOfEvents />
       </div>
--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+import { mockData } from "./mock-data";
+
 /**
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
@@ -8,4 +10,8 @@ export const extractLocations = (events) => {
   let extractLocations = events.map((event) => event.location);
   let locations = [...new Set(extractLocations)];
   return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
 };

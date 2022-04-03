52a2bb16ad0b30f52878f0b2fa8dd8e8c6db268f
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-04-03 12:42:23 +0100

    Implemented erroralert to numberofevents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import ErrorAlert from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
+    infoText: null,
   };
 
   handleInputChanged = (event) => {
@@ -11,9 +13,10 @@ class NumberOfEvents extends Component {
     if (value < 1 || value > 32) {
       this.setState({
         numberOfEvents: 32,
+        infoText: "Please insert a number between 1 and 32.",
       });
     } else {
-      this.setState({ numberOfEvents: value });
+      this.setState({ numberOfEvents: value, infoText: "" });
       this.props.updateNumberOfEvents(event.target.value);
     }
   };
@@ -21,6 +24,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="NumberOfEvents">
+        <ErrorAlert text={this.state.infoText} />
         <input
           type="number"
           className="numberOfEvents"
